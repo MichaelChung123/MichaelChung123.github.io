b1a2403c7ce512386656c617a56d6e68038dffe0
MichaelChung123 <michael.chung1994@gmail.com>
2020-03-18 12:32:12 -0700

    test master

---
+++ precache-manifest.a580ecaac57a305e8b8831bead7c7889.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c72b7c305ed68efa5bf6",
+    "url": "/static/css/main.c57041fa.chunk.css"
+  },
+  {
+    "revision": "c72b7c305ed68efa5bf6",
+    "url": "/static/js/main.c2475dff.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "eff2775d31097d3060fe",
+    "url": "/static/css/2.63424f38.chunk.css"
+  },
+  {
+    "revision": "eff2775d31097d3060fe",
+    "url": "/static/js/2.430db874.chunk.js"
+  },
+  {
+    "revision": "7ee02fe2c5a4bf188beeb3eae6cb2d91",
+    "url": "/static/media/ontime.7ee02fe2.png"
+  },
+  {
+    "revision": "927338d5550c829fcf5a5b3695482769",
+    "url": "/static/media/portrait.927338d5.jpg"
+  },
+  {
+    "revision": "b7a13198ffc40a129eea765008ff9095",
+    "url": "/static/media/bjuk.b7a13198.png"
+  },
+  {
+    "revision": "bde300d68b5e077aa9fb609f96863a13",
+    "url": "/static/media/bg-masthead.bde300d6.jpg"
+  },
+  {
+    "revision": "db4f204c18a38ad9da7ce896a9282d5f",
+    "url": "/index.html"
+  }
+];
